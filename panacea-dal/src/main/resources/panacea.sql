d5663b5fe085996b72ff539de74b168d529f7ca8
yunshang_734 <yunshang_734@163.com>
2023-08-21 14:50:14 +0800

    Add sql

--- panacea-dal/src/main/resources/panacea.sql
+++ panacea-dal/src/main/resources/panacea.sql
@@ -140,3 +140,22 @@ CREATE TABLE `fitness_user_clock_detail`
     `status`      TINYINT     NOT NULL DEFAULT '0' COMMENT '状态，0：未完成；1：已完成',
     PRIMARY KEY (id) USING BTREE
 ) ENGINE='INNODB' DEFAULT CHARSET='UTF8MB4' AUTO_INCREMENT=0 COMMENT='健身-用户打卡细节';
+
+
+
+CREATE TABLE `photography_user`
+(
+    `id`               BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `deleted`          TINYINT       NOT NULL DEFAULT '0' COMMENT '是否已删除，0：否；1：是',
+    `creator`          VARCHAR(16)   NOT NULL DEFAULT 'system' COMMENT '创建者',
+    `create_time`      DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `modifier`         VARCHAR(16)   NOT NULL DEFAULT 'system' COMMENT '修改者',
+    `modify_time`      DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    `username`         VARCHAR(256)  NOT NULL DEFAULT '' COMMENT '用户名',
+    `password`         VARCHAR(1024) NOT NULL DEFAULT '' COMMENT '密码',
+    `nickname`         VARCHAR(256)  NOT NULL DEFAULT '' COMMENT '昵称',
+    `gender`           TINYINT       NOT NULL DEFAULT '0' COMMENT '性别，1：男性；2：女性',
+    `first_login_time` DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '首次登录时间',
+    `last_login_time`  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最新登录时间',
+    PRIMARY KEY (id) USING BTREE
+) ENGINE='INNODB' DEFAULT CHARSET='UTF8MB4' AUTO_INCREMENT=0 COMMENT='摄影-用户';
